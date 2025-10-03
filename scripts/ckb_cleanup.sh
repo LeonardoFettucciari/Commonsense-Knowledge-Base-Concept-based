622b8deea56317738ebf87dbf8a8fbe74479ff72
Leonardo Fettucciari <fettucciari.1858879@studenti.uniroma1.it>
2025-10-04 00:01:28 +0200

    Cleanup

--- scripts/ckb_cleanup.sh
+++ scripts/ckb_cleanup.sh
@@ -1,3 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Default values
+INPUT_PATH="data/ckb/raw/kb.jsonl"
+
+# Parse long flags
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    --input_path) INPUT_PATH="$2"; shift ;;
+    *) echo "Unknown parameter passed: $1"; exit 1 ;;
+  esac
+  shift
+done
+
+echo "Using:"
+echo "Input path: $INPUT_PATH"
+
+# Run Python script
 python src/ckb_management/ckb_cleanup.py \
-    --input_path "data/ckb/raw/mergemerged.jsonl"
+  --input_path "$INPUT_PATH"
